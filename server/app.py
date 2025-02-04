37efdb5c1e4fff082bedbef688922d58b3d465d5
Kelly Hum <kellyhum88@gmail.com>
2025-02-03 23:31:16 -0800

    add remove and modify add to account for wishlist

--- server/app.py
+++ server/app.py
@@ -15,7 +15,7 @@ RestClient.configure(os.getenv('POKEMONTCG_IO_API_KEY'))
 # ROUTES
 # search cards
 @app.route('/search', methods=['GET','POST'])
-def search():
+def searchCards():
     query = request.get_json()
     data = query.get('query', '') # get the query otherwise default to empty
     
@@ -29,40 +29,67 @@ def search():
             "id": card.id,
             "name": card.name,
             "image": card.images.large,
-            "price": card.tcgplayer
         })
 
     return jsonify({'cards': min_attribute_cards})
 
 # add card to user entity 
 @app.route('/add', methods=['GET','POST'])
-def addCardtoDB():
+def addCard():
     query = request.get_json()
     cardValue = query.get('cardValue', '')
-    id, name, image, _ = cardValue # destructure while ignoring price
-    # print(cardValue[id])
+    id, name, image = cardValue # destructure
 
-    start_db.add_card(cardValue[id], cardValue[name], cardValue[image])
+    whichlist = query.get('whichList', '')
+
+    start_db.add_card(cardValue[id], cardValue[name], cardValue[image], whichlist)
 
     return jsonify({'msg':'added to database'})
 
 # get cards
 @app.route('/getCard')
-def getCardfromDB():
-    cards = start_db.pull_cards()
-    formatted_cards = []
+def getCard():
+    unformatted_collection_cards, unformatted_wishlist_cards = start_db.pull_cards()
+    collection_cards = []
+    wishlist_cards = []
 
-    for card in cards:
-        formatted_cards.append({
+    for card in unformatted_collection_cards:
+        collection_cards.append({
             "id": card[0],
             "name": card[1],
             "image": card[2],
         })
         print(card)
+    
+    for card in unformatted_wishlist_cards:
+        wishlist_cards.append({
+            "id": card[0],
+            "name": card[1],
+            "image": card[2],
+        })
+        print("wishlist")
+        print(card)
 
-    return jsonify({"cards": formatted_cards})
+    return jsonify({"collection": collection_cards, 
+                    "wishlist": wishlist_cards})
 
 # delete card
+@app.route('/remove', methods = ['GET', 'POST'])
+def remove():
+    query = request.get_json()
+    cardValue = query.get('cardValue', '')
+    id, _, _ = cardValue # destructure -> ignore everything except id
+
+    print("Cardvalue = " + cardValue[id])
+
+    try:
+        start_db.remove_card(cardValue[id])
+        print('removed')
+        return 'card removed successfully'
+    except Exception as e:
+        print(e)
+
+        return 'error in removing card'
 
 # set default card
 @app.route('/')
